0fbfb2c11e6a9fd690c2919d832ae4ffc21d2e79
Jon Barclay <my@nameisjon.com>
2019-11-10 16:57:34 -0600

    junction table added to sql

--- database.sql
+++ database.sql
@@ -13,9 +13,10 @@ CREATE TABLE "genres" (
 
 
 -- CREATE JUNCTION TABLE
--- You will need to create the junction table that stores the relationships between "movies" and "genres"
--- This table will need to be populated with some data as well (INSERTS)
--- Recall that this Junction Table will just be a table of ids!
+CREATE TABLE "movies_genres" (
+	"movie_id" INT REFERENCES "movies",
+	"genre_id" INT REFERENCES "genres"
+);
 
 
 
@@ -55,3 +56,39 @@ VALUES
 ('Science Fiction'),
 ('Space-Opera'),
 ('Superhero');
+
+-- starter genre junctions
+INSERT INTO "movies_genres" ("movie_id", "genre_id")
+VALUES ('1', '1'),
+('1', '2'),
+('1', '8'),
+('2', '8'),
+('2', '9'),
+('3', '1'),
+('3', '11'),
+('3', '13'),
+('4', '1'),
+('4', '2'),
+('4', '4'),
+('5', '6'),
+('6', '1'),
+('6', '8'),
+('7', '1'),
+('8', '1'),
+('8', '6'),
+('8', '8'),
+('9', '1'),
+('9', '2'),
+('9', '4'),
+('10', '1'),
+('10', '8'),
+('11', '1'),
+('11', '6'),
+('11', '11'),
+('12', '3'),
+('12', '6'),
+('13', '6'),
+('13', '10'),
+('14', '1'),
+('14', '2'),
+('14', '4');
